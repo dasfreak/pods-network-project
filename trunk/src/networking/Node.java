56b9c629502f531c3077d618287f34bd6fcf5884
elias3 <elias3@fc41d4f1-24ae-187d-393a-ea6c9657c264>
2014-11-16 20:46:31 +0000

    - initial skeleton for Node class that contains IP and a list of IPs resembling the network - added Apache XML rpc to classpath and the jar to the project repository.

---
+++ trunk/src/networking/Node.java
@@ -0,0 +1,80 @@
+package networking;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.xmlrpc.client.XmlRpcClient;
+
+public class Node {
+	InetAddress ip;
+	List<InetAddress> network;
+	private static Node node;
+	
+	private Node(){
+		try {
+			ip = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		network = new LinkedList<InetAddress>();
+	}
+	
+	public static Node getInstance(){
+		if ( null == node )
+		{
+			node = new Node();
+		}
+		return node;
+	}
+	
+	public void joinWithPropagate( InetAddress ip ) // a.k.a broadcast
+	{
+		for ( InetAddress addr : network )
+		{
+			sendJoinNetworkOnlyRequest( ip );
+		}
+		network.add(ip);
+	}
+	
+	public void removeRequestReceived( InetAddress ip )
+	{
+		network.remove( ip );
+	}
+	
+	public void disconnect()
+	{
+		for ( InetAddress addr : network )
+		{
+			sendDisconnectRequest( addr );
+		}
+	}
+	
+	public void sendConnectRequest( InetAddress knownIPinNetwork )
+	{
+		// send the request ...
+	}
+
+	private void sendDisconnectRequest(InetAddress addr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void sendJoinNetworkOnlyRequest(InetAddress ip) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void joinNetworkOnly( InetAddress ip )
+	{
+		network.add(ip);
+	}
+	
+	public static void main(String[] args)  {
+		System.out.println("IP = "+Node.getInstance().ip);
+	}
+	
+}
+
