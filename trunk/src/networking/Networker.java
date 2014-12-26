8cc10368b8297456068f5dbab56a992731b73197
elias3 <elias3@fc41d4f1-24ae-187d-393a-ea6c9657c264>
2014-12-26 16:06:30 +0000

    changed mechanism of ip fetching

--- trunk/src/networking/Networker.java
+++ trunk/src/networking/Networker.java
@@ -1,14 +1,36 @@
 package networking;
 
+import static java.lang.System.out;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.Enumeration;
 
 public class Networker {
 
 	public static void main(String[] args) throws UnknownHostException, SocketException {
 
+        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+        for (NetworkInterface netint : Collections.list(nets))
+            displayInterfaceInformation(netint);
+
+        
 		new Server();
 		Client client = new Client();
 		new Thread( client ).start();
 	}
+	
+    static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+        out.printf("Display name: %s\n", netint.getDisplayName());
+        out.printf("Name: %s\n", netint.getName());
+        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+            out.printf("InetAddress: %s\n", inetAddress);
+        }
+        out.printf("\n");
+     }
+
 }
