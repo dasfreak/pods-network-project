b2072f4e29da46005efcd8724362474b24b72c33
elias3 <elias3@fc41d4f1-24ae-187d-393a-ea6c9657c264>
2014-12-26 15:15:44 +0000

    handler for network propogation

--- trunk/src/networking/ClientAux.java
+++ trunk/src/networking/ClientAux.java
@@ -1,13 +1,14 @@
 package networking;
 
 public class ClientAux {
-	 public static void newNodeInNetwork(String ip) {
+	 public static boolean newNodeInNetwork(String ip) {
 		 try {
 			Client.getInstance().addNodeToStructure(ip);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return true;
 		 
 	 }
 }
