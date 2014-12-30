54c63afac90f10a712a1b5c182cda2e6a58e91d5
marc.ohm89@gmail.com <marc.ohm89@gmail.com@fc41d4f1-24ae-187d-393a-ea6c9657c264>
2014-12-30 11:59:06 +0000

    small C# server+client (execute as admin)

---
+++ CSharp/xmlrpc/xmlrpc/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.Samples.XmlRpc;
+
+namespace xmlrpc
+{
+
+    // describe your service's interface here
+    [ServiceContract()]
+    interface IMath
+    {
+        [OperationContract()]
+        double Add(double A, double B);
+    }
+
+    public class Math : IMath
+    {
+        public double Add(double A, double B)
+        {
+            return A + B;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Starting Server");
+
+            Uri baseAddress = new UriBuilder(Uri.UriSchemeHttp, "localhost", -1, "/xmlrpc").Uri;
+            ServiceHost serviceHost = new ServiceHost(typeof(Math));
+            var epXmlRpc = serviceHost.AddServiceEndpoint(typeof(IMath), new WebHttpBinding(WebHttpSecurityMode.None), new Uri(baseAddress, "./xmlrpc"));
+            epXmlRpc.Behaviors.Add(new XmlRpcEndpointBehavior());
+
+            serviceHost.Open();
+
+            Console.WriteLine("listening at: {0}", epXmlRpc.ListenUri);
+
+
+            Console.WriteLine("Starting Client");
+
+            ChannelFactory<IMath> cf = new ChannelFactory<IMath>(new WebHttpBinding(), "http://localhost/xmlrpc");
+
+            cf.Endpoint.Behaviors.Add(new XmlRpcEndpointBehavior());
+
+            IMath client = cf.CreateChannel();
+
+            // you can now call methods from your remote service
+            double answer = client.Add(1,5);
+            Console.WriteLine("Sending 1+5...");
+            Console.WriteLine("Result: "+answer.ToString());
+            Console.ReadLine();
+        }
+    }
+}
