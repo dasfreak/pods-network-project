9c393f7e0596c39ae3d971c313e889c38b8abfc5
elias3 <elias3@fc41d4f1-24ae-187d-393a-ea6c9657c264>
2015-01-10 17:04:15 +0000

    created Thread CalculatingTask for the "20 seconds" calc session

---
+++ src/networking/CalculatingTask.java
@@ -0,0 +1,42 @@
+package networking;
+
+import java.util.Random;
+
+public class CalculatingTask implements Runnable {
+
+	static final int TIME_FOR_CALC_IN_MSEC    = 20 * 1000;
+	static final int MAX_TIME_FOR_WAIT_IN_MSC =  1 * 1000;
+
+	@Override
+	public void run() {
+		long timeStart = System.currentTimeMillis();
+		long currentTime = 0;
+	    Random randomGenerator = new Random();
+	    long randomTimeInMSec;
+		System.out.println("Starting calc session for: "+TIME_FOR_CALC_IN_MSEC/1000+" seconds:");
+		do
+		{			
+			Operation op = Operation.values()[randomGenerator.nextInt(Operation.values().length)];
+			int genNumber = randomGenerator.nextInt(100);
+			System.out.println("["+System.currentTimeMillis()+"] Initiating calculation: Operation: "+op+" Value: "+genNumber);
+			try {
+				Client.getInstance().performCalc( op, genNumber );
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			// wait a random time	
+			randomTimeInMSec = randomGenerator.nextInt(MAX_TIME_FOR_WAIT_IN_MSC);
+			try {
+				Thread.sleep(randomTimeInMSec);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			currentTime = System.currentTimeMillis();
+		} while ( ( currentTime - timeStart ) < TIME_FOR_CALC_IN_MSEC );
+		System.out.println("The time is up!");
+	}
+	
+}
