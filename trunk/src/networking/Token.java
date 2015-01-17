da89f129850c7e39b3eb51ab6af1fdfdd4d3b201
elias3 <elias3@fc41d4f1-24ae-187d-393a-ea6c9657c264>
2015-01-17 10:56:49 +0000

    token ring initial implementation

---
+++ trunk/src/networking/Token.java
@@ -0,0 +1,11 @@
+package networking;
+
+public class Token {
+		String ipCreator;
+		String currentHolder;
+		
+		public Token (String ipCreator, String currentHolder) {
+			this.ipCreator      = ipCreator;
+			this.currentHolder  = currentHolder;
+		}
+}
--- trunk/src/networking/TokenRing.java
+++ trunk/src/networking/TokenRing.java
@@ -1,31 +1,32 @@
 package networking;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Vector;
+
+import org.apache.xmlrpc.XmlRpcException;
 
 import networking.Client.RemoteNode;
 
 public class TokenRing implements Runnable {
-	
-	class Token{
-		String ipCreator;
-		String currentHolder;
-		int    indexOfHolderInRing;
 		
-		public Token (String ipCreator, String currentHolder) {
-			this.ipCreator      = ipCreator;
-			this.currentHolder  = currentHolder;
-			indexOfHolderInRing = 0;
-		}
-	}
+	private static TokenRing instance = null;
 	
 	private List<RemoteNode> network;
+	private int indexInRing = -1;
+	
 	private String ip;
 	private String ipCordinator;
 	
 	private Token token = null;
+	
+	public static TokenRing getInstance()
+	{
+		return instance;
+	}
 	
 	public TokenRing(List<RemoteNode> network, String ip)
 	{
@@ -33,10 +34,21 @@ public class TokenRing implements Runnable {
 		this.network.addAll(network);
 		this.ip      = ip;
 		
+		Collections.sort(network);
+		
+		for ( int index = 0; index < network.size(); index++ )
+		{
+			if ( network.get(index).ip.compareTo(this.ip) == 0 )
+			{
+				this.indexInRing = index;
+				break;
+			}
+		}
+		
 		fetchCordinator();
+		this.instance = this;
 	}
 	private void fetchCordinator() {
-		Collections.sort(network);
 		ipCordinator = network.get(0).ip;
 		if ( ipCordinator.compareTo(this.ip) == 0 )
 		{
@@ -52,9 +64,9 @@ public class TokenRing implements Runnable {
 	public void run() {
 		while (true)
 		{
-			if ( isHolderOfRing() && isCalcDone() )
+			if ( isHolderOfToken() && isCalcDone() )
 			{
-				
+				forwardToken();
 			}
 		}
 	}
@@ -62,12 +74,40 @@ public class TokenRing implements Runnable {
 		// TODO Auto-generated method stub
 		return true;
 	}
-	private boolean isHolderOfRing() {
+	private boolean isHolderOfToken() {
 		if ( token != null && token.currentHolder.compareTo(this.ip) == 0 )
 		{
 			return true;
 		}
 		return false;
+	}
+	
+	private void forwardToken()
+	{
+		// fetch next peer:
+		int nextPeer = ( indexInRing + 1 ) % network.size();
+		System.out.println("Forwarding token to IP "+network.get(nextPeer).ip);
+
+		Vector<String> params = new Vector<String>();
+
+		params.add(token.ipCreator);
+		params.add(network.get(nextPeer).ip);
+		
+		token = null;
+		
+		// send request to node
+		try {
+			network.get(nextPeer).rpc.execute("TokenRingAux.tokenReceived", params );
+		} catch (XmlRpcException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void receivedToken(Token token)
+	{
+		System.out.println("Received Token!");
+		this.token = token;
 	}
 
 }
